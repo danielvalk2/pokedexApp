783a89d6e5a709b52258aa1b77812d35b4c47372
danieel32 <110873554+danieel32@users.noreply.github.com>
2022-10-11 09:21:41 -0700

    Add files via upload

---
+++ pages/_app.js
@@ -0,0 +1,14 @@
+import '../styles/globals.css'
+import Head from "next/head";
+
+function MyApp({ Component, pageProps }) {
+  return (
+  <>
+    <Head>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous"></link>
+    </Head>
+    return <Component {...pageProps} />
+  </>);
+}
+
+export default MyApp
---
+++ pages/daniel.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Daniel() {
+  return <div>
+    <p>Daniel</p>
+    <Link href="/">
+        <a>back</a>
+    </Link>
+  </div>
+}
