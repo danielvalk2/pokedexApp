877a90490f429854bd10be528fb9649a4de8d4bd
danieel32 <110873554+danieel32@users.noreply.github.com>
2023-01-09 12:23:24 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ function Card(props) {
   const [likes, setLikes] = useState(0);
 
   return (
-    <div className="card col-4 d-flex justify-content-center d-block p-2 text-bg-secondary">
+    <div className="card col-4 d-flex justify-content-center d-block p-2 text-bg-success">
       <img src={props.src} className="card-img-top" alt="..." />
       <div className="card-body">
         <Link href={{pathname: "pokemons/[id]", query: {id: props.id}}}>
