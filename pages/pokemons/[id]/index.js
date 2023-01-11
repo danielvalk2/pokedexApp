6c3e26a9756204eb843fd30d4f8f94c0ba3305f2
danieel32 <110873554+danieel32@users.noreply.github.com>
2023-01-11 15:30:04 -0800

    Update index.js

--- pages/pokemons/[id]/index.js
+++ pages/pokemons/[id]/index.js
@@ -30,7 +30,7 @@ export default function Pokemon() {
                 </div>
             ): null}
             {pokemon ? (
-                <div className="card text-bg-success" id="info" style={{position: "relative"}}>
+                <div className="card text-bg-success" id="card-img-top" style={{position: "relative"}}>
                     <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`} className="Info" alt="..." style={{ height: "300px", width: "300px"}}/>
                     <div className="card-body text-center">
                         <h5 className="card-title">{pokemon.name}</h5>
