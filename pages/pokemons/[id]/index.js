79522f05aafb6cd11cb7697dbce9adb564f1b906
danieel32 <110873554+danieel32@users.noreply.github.com>
2023-01-11 15:24:51 -0800

    Update index.js

--- pages/pokemons/[id]/index.js
+++ pages/pokemons/[id]/index.js
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
 import Link from "next/link";
+import styles from '../../../styles/Home.module.css'
 
 export default function Pokemon() {
     const [pokemon, setPokemon] = useState(null);
