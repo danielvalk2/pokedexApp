8dabcfa7d0b7d7d5ba48ef7136d475275f6e2180
danieel32 <110873554+danieel32@users.noreply.github.com>
2023-01-12 13:13:27 -0800

    Update index.js

--- pages/pokemons/[id]/index.js
+++ pages/pokemons/[id]/index.js
@@ -12,7 +12,7 @@ export default function Pokemon() {
 
     useEffect(() => {
         if (!router.isReady) return;
-        fetch(`https://pokeapi.co/api/v2/evolution-chain/${id}`)
+        fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
             .then(response => {
                 return response.json();
             })
@@ -34,7 +34,7 @@ export default function Pokemon() {
                     <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`} alt="..."/>
                     <div className="card-body text-center">
                         <h5 className="card-title">{pokemon.name}</h5>
-                        <p className="card-text">levels: {evolution.min_level}</p>
+                        <p className="card-text">levels: {pokemon.base_experience}</p>
                         <Link href="/">
                             <a className="btn btn-dark">
                                 back
